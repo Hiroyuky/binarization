2509555bbb4f19f0aea2ae48bc8ebab4c6c58597
Hiro <smile.cocoai@gmail.com>
2015-10-21 17:41:42 +0900

    binarization

--- binarization.py
+++ binarization.py
@@ -1,27 +1,22 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-画像をグレースケール化し，そこからしきい値を設けて0or1の2値化処理します
-出力はテキストデータ
-
-githubって便利ですね☆
-
-"""
 
 from PIL import Image
 import numpy as np
 from matplotlib import pylab as plt
 
-def binari(i, j):
-	
+def binari(k_max, d_max):
 	FILENAME_IN_DATA = 'DATA_pic/'		# 変換する画像ディレクトリ名
 	FILENAME_OUT_DATA = 'DATA_binari/'	# 出力するディレクトリ名
 	FILENAME_IN_EXTENSION = '.bmp'		# 入力ファイル拡張子
 	FILENAME_OUT_EXTENSION = '.csv'		# 出力ファイル拡張子
 
-
 	# 扱う画像データ名
-	PIC_NAME = str(i) + '_' + str(j)
+	PIC_NAME = str(k_max) + '_' + str(d_max)
+	print 'input file name: ' + PIC_NAME
+
+	# 圧縮率
+	comp = 10
 
 	# 入力するファイル名統合
 	picName = FILENAME_IN_DATA + PIC_NAME + FILENAME_IN_EXTENSION
@@ -33,7 +28,7 @@ def binari(i, j):
 	img = np.array(Image.open(picName).convert('L'))
 
 	# 1/10にする（300x300 -> 30x30）
-	img = (img[::10, ::10] + img[1::10, ::10] + img[::10, 1::10] + img[1::10, 1::10])/4
+	img = (img[::comp, ::comp] + img[1::comp, ::comp] + img[::comp, 1::comp] + img[1::comp, 1::comp])/4
 
 	f = open(binariName, "w")
 
@@ -44,16 +39,16 @@ def binari(i, j):
 			else:
 				f.write("1")
 			f.write(",")
-
-		# スカラーにするときは↓をコメントアウト
 	f.write("\n")
+
+	print 'success!'
 
 if __name__ == '__main__':
 	K_MAX = 3	# クラス数
 	D_MAX = 5	# データ数
-
-	for i in range(0, K_MAX):
-		for j in range(0, D_MAX):
-			binari(i, j)
+	print 'Pic to binarization'
+	for k in range(0, K_MAX):
+		for d in range(0, D_MAX):
+			binari(k, d)
 
 
